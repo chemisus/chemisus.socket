423552753ec00c0e8470645494dd12417b01094a
Terrence Howard <chemisus@gmail.com>
2012-04-24 18:30:15 -0400

    added comments

--- src/chemisus/socket/SocketInputStream.java
+++ src/chemisus/socket/SocketInputStream.java
@@ -44,12 +44,34 @@ public class SocketInputStream
     /*\**********************************************************************\*/
     /*\                             Properties                               \*/
     /*\**********************************************************************\*/
+    /**
+     * Get the amount of bytes available in the stream for reading.
+     * 
+     * @return <b>int</b> The amount of bytes available in the stream for
+     * reading.
+     * @throws IOException 
+     */
     @Override
     public int available() throws IOException
     {
         return input.available();
     }
 
+    /**
+     * Gets if marking is supported by the stream.
+     * 
+     * @return <b>boolean</b>
+     * <table>
+     *  <tr>
+     *      <td><i>true</i></td>
+     *      <td>Marking is supported by the stream.</td>
+     *  </tr>
+     *  <tr>
+     *      <td><i>false</i></td>
+     *      <td>Marking is not supported by the stream.</td>
+     *  </tr>
+     * </table>
+     */
     @Override
     public boolean markSupported()
     {
@@ -77,22 +99,50 @@ public class SocketInputStream
     /*\**********************************************************************\*/
     /*\                             Public Methods                           \*/
     /*\**********************************************************************\*/
+    /**
+     * Reads a <b>byte</b> from the stream.
+     * 
+     * @return <b>int</b> the value of the byte that was read from the stream.
+     * @throws IOException 
+     */
     @Override
     public int read() throws IOException
     {
         return input.read();
     }
-    
+
+    /**
+     * Reads a <b>Boolean</b> from the stream.
+     * 
+     * @return <b>Boolean</b> the value of the Boolean read from the stream.
+     * @throws IOException 
+     */
     public Boolean readBoolean() throws IOException
     {
         return input.readBoolean();
     }
     
+    /**
+     * Reads a <b>Byte</b> from the stream.
+     * 
+     * @return <b>Byte</b> the value of the Byte read from the stream.
+     * @throws IOException 
+     */
     public Byte readByte() throws IOException
     {
         return input.readByte();
     }
     
+    /**
+     * Reads a <b>byte[]</b> from the stream.
+     * 
+     * Since <b>SocketOutputStream</b> will send the size of the byte array 
+     * before the bytes themselves, this function will automatically read in
+     * that size <i>n</i> first, then read in the following <i>n</i> bytes.
+     * 
+     * @return <b>byte[]</b> the value of the byte[] read from the stream.
+     * @throws IOException 
+     */
     public byte[] readBytes() throws IOException
     {
         byte[] bytes = new byte[readInt()];
@@ -102,64 +152,149 @@ public class SocketInputStream
         return bytes;
     }
     
+    /**
+     * Reads a <b>Character</b> from the stream.
+     * 
+     * @return <b>Character</b> the value of the Character read from the stream.
+     * @throws IOException 
+     */
     public Character readChar() throws IOException
     {
         return input.readChar();
     }
     
+    /**
+     * Reads a <b>Double</b> from the stream.
+     * 
+     * @return <b>Double</b> the value of the Double read from the stream.
+     * @throws IOException 
+     */
     public Double readDouble() throws IOException
     {
         return input.readDouble();
     }
     
+    /**
+     * Reads a <b>Float</b> from the stream.
+     * 
+     * @return <b>Float</b> the value of the Float read from the stream.
+     * @throws IOException 
+     */
     public Float readFloat() throws IOException
     {
         return input.readFloat();
     }
     
+    /**
+     * Reads a <b>Integer</b> from the stream.
+     * 
+     * @return <b>Integer</b> the value of the Integer read from the stream.
+     * @throws IOException 
+     */
     public Integer readInt() throws IOException
     {
         return input.readInt();
     }
     
+    /**
+     * Reads a <b>Long</b> from the stream.
+     * 
+     * @return <b>Long</b> the value of the Long read from the stream.
+     * @throws IOException 
+     */
     public Long readLong() throws IOException
     {
         return input.readLong();
     }
     
+    /**
+     * Reads a <b>Short</b> from the stream.
+     * 
+     * @return <b>Short</b> the value of the Short read from the stream.
+     * @throws IOException 
+     */
     public Short readShort() throws IOException
     {
         return input.readShort();
     }
     
+    /**
+     * Reads a <b>String</b> from the stream.
+     * 
+     * This function returns a new string by calling
+     * <code>new String(readBytes())</code>. Since <b>SocketOutputStream</b>
+     * will send the size of the string before the string itself, this function
+     * will automatically read in that size first.
+     * 
+     * @return <b>String</b> the value of the String read from the stream.
+     * @throws IOException 
+     */
     public String readString() throws IOException
     {
         return new String(readBytes());
     }
     
+    /**
+     * Closes the stream.
+     * 
+     * @throws IOException 
+     */
     @Override
     public void close() throws IOException
     {
         input.close();
     }
-    
+
+    /**
+     * Marks the current position in the stream until the <i>limit<i> number of
+     * bytes have been read from the stream.
+     * 
+     * This function saves a spot in the stream, and when <code>reset()</code>
+     * is called on the stream, it will go back to the position.
+     * 
+     * If however, when <i>limit<i> number of bytes have been read from the
+     * stream, the mark will become invalid.
+     * 
+     * @param limit The number of bytes to be read before the mark becomes
+     * invalid.
+     */
     @Override
     public void mark(int limit)
     {
         input.mark(limit);
     }
     
+    /**
+     * Skips <i>n</i> bytes in the stream.
+     * 
+     * @param n The number of bytes to skip in the stream.
+     * @return <b>long</b> The actual number of bytes skipped.
+     * @throws IOException 
+     */
     @Override
     public long skip(long n) throws IOException
     {
         return input.skip(n);
     }
-    
+
+    /**
+     * Skips <i>n</i> bytes in the stream.
+     * 
+     * @param n The number of bytes to skip in the stream.
+     * @return <b>long</b> The actual number of bytes skipped.
+     * @throws IOException 
+     */
     public int skipBytes(int n) throws IOException
     {
         return input.skipBytes(n);
     }
-    
+
+    /**
+     * Sets the position in the stream to the index of the last time a mark was
+     * called, provided that it is still valid.
+     * 
+     * @throws IOException 
+     */
     @Override
     public void reset() throws IOException
     {
--- src/chemisus/socket/SocketOutputStream.java
+++ src/chemisus/socket/SocketOutputStream.java
@@ -71,12 +71,34 @@ public class SocketOutputStream
     /*\**********************************************************************\*/
     /*\                             Public Methods                           \*/
     /*\**********************************************************************\*/
+    /**
+     * Writes a byte to the stream.
+     * 
+     * Note that the parameter is an <b>int</b> (bigger size than a byte). This
+     * function will write a <b>byte</b> to the stream.
+     * 
+     * @param value The value of the byte to write to the stream.
+     * @throws IOException 
+     */
     @Override
     public void write(int value) throws IOException
     {
         output.write(value);
     }
 
+    /**
+     * Writes several bytes to the stream.
+     * 
+     * This function calls <code>writeInt(value.length)</code> before writing
+     * the bytes to the stream. This extra <b>int</b> is automatically read by
+     * the <code>SocketInputStream.readInt()</code> on the other end of the
+     * stream.
+     * 
+     * @param value The array of bytes to write from.
+     * @param offset The index of the byte in the array to start from.
+     * @param length The number of bytes to write to the stream.
+     * @throws IOException 
+     */
     @Override
     public void write(byte[] value, int offset, int length) throws IOException
     {
@@ -84,69 +106,159 @@ public class SocketOutputStream
         
         output.write(value, 0, value.length);
     }
-    
+
+    /**
+     * Writes an array of bytes to the stream.
+     * 
+     * This function calls <code>write(value, 0, value.length)</code>;
+     * 
+     * @param value The array of bytes to write to the stream.
+     * @throws IOException 
+     */
     @Override
     public void write(byte[] value) throws IOException
     {
         write(value, 0, value.length);
     }
-    
+
+    /**
+     * Writes a <b>Boolean</b> to the stream.
+     * 
+     * @param value The <b>Boolean</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeBoolean(Boolean value) throws IOException
     {
         output.writeBoolean(value);
     }
 
+    /**
+     * Writes a <b>Byte</b> to the stream.
+     * 
+     * @param value The <b>Byte</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeByte(Byte value) throws IOException
     {
-        write(value);
+        output.writeByte(value);
     }
 
+    /**
+     * Writes a <b>byte[]</b> to the stream.
+     * 
+     * This function calls <code>writeBytes(value.getBytes())</code>, which
+     * means that the size of the string will be written to the stream before
+     * the actual bytes of the array. This extra <b>int</b> will be
+     * automatically read by the <b>SocketInputStream</b> when using
+     * <code>readBytes()</code>.
+     * 
+     * @param value The <b>byte[]</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeBytes(byte[] value) throws IOException
     {
         write(value);
     }
     
+    /**
+     * Writes a <b>Character</b> to the stream.
+     * 
+     * @param value The <b>Character</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeChar(Character value) throws IOException
     {
         output.writeChar(value);
     }
 
+    /**
+     * Writes a <b>Double</b> to the stream.
+     * 
+     * @param value The <b>Double</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeDouble(Double value) throws IOException
     {
         output.writeDouble(value);
     }
 
+    /**
+     * Writes a <b>Float</b> to the stream.
+     * 
+     * @param value The <b>Float</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeFloat(Float value) throws IOException
     {
         output.writeFloat(value);
     }
 
+    /**
+     * Writes a <b>Integer</b> to the stream.
+     * 
+     * @param value The <b>Integer</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeInt(Integer value) throws IOException
     {
         output.writeInt(value);
     }
     
+    /**
+     * Writes a <b>Long</b> to the stream.
+     * 
+     * @param value The <b>Long</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeLong(Long value) throws IOException
     {
         output.writeLong(value);
     }
 
+    /**
+     * Writes a <b>Short</b> to the stream.
+     * 
+     * @param value The <b>Short</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeShort(Short value) throws IOException
     {
         output.writeShort(value);
     }
 
+    /**
+     * Writes a <b>String</b> to the stream.
+     * 
+     * This function calls <code>writeBytes(value.getBytes())</code>, which
+     * means that the size of the string will be written to the stream before
+     * the actual bytes of the string. This extra <b>int</b> will be
+     * automatically read by the <b>SocketInputStream</b> when using
+     * <code>readString()</code>.
+     * 
+     * @param value The <b>String</b> value to write to the stream.
+     * @throws IOException 
+     */
     public void writeString(String value) throws IOException
     {
         writeBytes(value.getBytes());
     }
     
+    /**
+     * Flush the stream.
+     * 
+     * @throws IOException 
+     */
     @Override
     public void flush() throws IOException
     {
         output.flush();
     }
-    
+
+    /**
+     * Close the stream.
+     * 
+     * @throws IOException 
+     */
     @Override
     public void close() throws IOException
     {
